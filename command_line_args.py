0ae66dabc1f8501c98778718416c0d0e47508a4d
Emil Haegglund <haegglund.emil@gmail.com>
2019-11-13 11:22:31 +0100

    Add command_line_args script

---
+++ command_line_args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+print('All arguments')
+print(sys.argv)
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+print('Argument 1: ' + arg1)
+print('Argument 2: ' + arg2)
